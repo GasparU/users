f84a11e35346a93972a741a65d2b33b78f1cf591
Jose Gaspar <sainc2016@gmail.com>
2023-06-03 18:37:43 -0500

    manejando name

--- src/hooks/useUserCrud.js
+++ src/hooks/useUserCrud.js
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
-// import {url_back} from "../../url"
 const useUserCrud = () => {
   const [users, setUsers] = useState();
   const url = `https://users-crud.academlo.tech/users`;
-  // const url = `${url_back.URL_USERS}`;
 
   // GET
 
